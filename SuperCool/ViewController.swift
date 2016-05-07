aa6046bae77b57c98a70ce3c4b21fe0beffbe734
KfirLS <kfir.levsinai@gmail.com>
2016-05-07 22:13:16 +0300

    added files for SuperCool

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Kfir Lev Sinai on 10/01/2016.
+//  Copyright © 2016 Kfir Lev Sinai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
